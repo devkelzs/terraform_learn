4fc882ea9da4e688f7055425fac6f5a142b557ea
devkelzs <nkwainkelly8@gmail.com>
2026-01-10 03:39:23 -0500

    Add ALB and ASG modules

--- main.tf
+++ main.tf
@@ -23,3 +23,22 @@ module "app" {
   name              = "app-server"
 }
 
+module "alb" {
+  source = "./modules/alb"
+
+  vpc_id             = module.vpc.vpc_id
+  public_subnet_ids  = module.vpc.public_subnet_ids
+  alb_sg_id          = module.vpc.alb_sg_id
+}
+
+module "asg" {
+  source = "./modules/asg"
+
+  ami               = var.ami
+  instance_type     = var.instance_type
+  subnet_ids        = module.vpc.private_subnet_ids
+  instance_sg_id    = module.vpc.web_sg_id
+  target_group_arn  = module.alb.target_group_arn
+  user_data         = file("${path.module}/user-data/web.sh")
+  name              = "web-asg"
+}
